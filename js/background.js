4283e1ac0734576eeb211e4ea0fe6b05859180af
apknusel <apknusel@stu.naperville203.org>
2020-10-06 15:36:42 -0500

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,41 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
+{
+    var grades = [];
+    var total = 0;
+    var counter = 0;
+    fetch("https://infinitecampus.naperville203.org/campus/resources/portal/grades").then(response=>response.json()).then(data=>
+    {
+        for (i = 0;i<data[0].courses.length;i++)
+        {
+            console.log(parseFloat(data[0].courses[i].gradingTasks[2].progressPercent));
+            grades.push(parseFloat(data[0].courses[i].gradingTasks[2].progressPercent));
+        }
+    sendResponse(grades);
+    })
+    grades = grades.filter(e=>!Number.isNaN(e));
+    for (i = 0; i < grades.length; i++)
+    {
+        if (grades[i] >= 89.5)
+        {
+            counter+=4;
+        }
+        else if (grades[i] >= 79.5)
+        {
+            counter+=3;
+        }
+        else if (grades[i] >= 69.5)
+        {
+            counter+=2;
+        }
+        else if (grades[i] >= 59.6)
+        {
+            counter+=1;
+        }
+        else
+        {
+            counter+=0;
+        }
+    }
+    total = counter/grades.length;
+    return total;
+});
---
+++ js/popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+document.getElementById('calcGPA').addEventListener('click', function()
+    {
+        chrome.tabs.query({currentWindow: true, active: true},
+            function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, 'calcGPA', calcGPA())
+            })
+    });
+});
+function calcGPA(total)
+{
+    console.log(total);
+}
